feeac889c148ca7a94163734545b06c2477733bf
sahith-sys <sahithkothuru@gmail.com>
2024-07-09 13:48:06 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 1701-average-waiting-time/1701-average-waiting-time.py
@@ -0,0 +1,27 @@
+"""class Solution:
+    def averageWaitingTime(self, customers: List[List[int]]) -> float:
+        start,finish,cus_wait,sums = 0,0,0,0
+        for i in range (len(customers)):
+            for j in range(2):
+                if i ==0:
+                    finish = finish + customers[i][j]
+                    cus_wait = finish -customers[i][i]
+                else:
+                    start = (start + customers[i][j])+((i+1)//2)
+                    finish = start + customers[i][1]
+                    cus_wait = finish - customers[i][0]
+                    sums = sums + cus_wait
+        return sums/len(customers)"""
+
+class Solution:
+    def averageWaitingTime(self, customers: List[List[int]]) -> float:
+        start,finish,cus_wait,sums = 0,0,0,0
+        
+        for cus in customers:
+            arr,timereq = customer
+            finish = max(finish, arr) + timereq
+            sums = sums + finish - arr
+            
+            
+        return sums/len(customers)
+        
