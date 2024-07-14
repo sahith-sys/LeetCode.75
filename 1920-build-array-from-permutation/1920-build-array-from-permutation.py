71c54dcc2bccfdcb377bca1e15ddc8f8193b4232
sahith-sys <sahithkothuru@gmail.com>
2024-07-14 11:10:26 +0530

    Time: 102 ms (20.12%), Space: 16.9 MB (19.82%) - LeetHub

--- 1920-build-array-from-permutation/1920-build-array-from-permutation.py
+++ 1920-build-array-from-permutation/1920-build-array-from-permutation.py
@@ -1,7 +1,15 @@
-class Solution:
+"""class Solution:
     def buildArray(self, nums: List[int]) -> List[int]:
         arr = []
         for i in range(len(nums)):
             arr.append(nums[nums[i]])
         return arr
+        """
+
+class Solution:
+    def buildArray(self, nums: List[int]) -> List[int]:
+        x = len(nums)
+        for i in range(x):
+            nums.append(nums[nums[i]])
+        return nums[x::]
         
